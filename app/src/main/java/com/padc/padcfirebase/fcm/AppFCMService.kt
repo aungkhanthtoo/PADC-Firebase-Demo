9e7b5408422972c22b4ec9ebc07d7a86422cae33
Htoo Aung Kyaw <htooaung.kyaw@mm-digital-solutions.com>
2019-11-23 11:37:17 +0630

    receiving foreground fcm with data payload.

--- app/src/main/java/com/padc/padcfirebase/fcm/AppFCMService.kt
+++ app/src/main/java/com/padc/padcfirebase/fcm/AppFCMService.kt
@@ -4,9 +4,10 @@ import android.app.PendingIntent
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.padc.padcfirebase.activities.DetailActivity
 import com.padc.padcfirebase.activities.MainActivity
 
-class AppFCMService: FirebaseMessagingService() {
+class AppFCMService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         super.onNewToken(token)
@@ -17,36 +18,44 @@ class AppFCMService: FirebaseMessagingService() {
         super.onMessageReceived(remoteMessage)
         Log.d(TAG, "onMessageReceived: ${remoteMessage.notification}")
 
+        var title: String? = null
+        var body: String? = null
+
+        val mainIntent = MainActivity.getNewIntent(this)
+        var pendingIntent = PendingIntent.getActivity(
+            this, 1, mainIntent, PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
         // Check if message contains a notification payload.
         remoteMessage.notification?.let {
             Log.d(TAG, "Message Notification Body: ${it.body}")
 
-            val mainIntent = MainActivity.getNewIntent(this)
-            val pendingIntent = PendingIntent.getActivity(
-                this, 1, mainIntent, PendingIntent.FLAG_UPDATE_CURRENT
-            )
+            title = it.title
+            body = it.body
 
-            buildAndSendNotification(
-                this,
-                it.title ?: "FCM Testing",
-                it.body ?: "FCM Testing",
-                pendingIntent
-            )
 
         }
 
         // Check if message contains a data payload.
-        remoteMessage.data.isNotEmpty().let {
+        if (remoteMessage.data.isNotEmpty()) {
+
             Log.d(TAG, "Message data payload: " + remoteMessage.data)
 
-            if (/* Check if data needs to be processed by long running job */ true) {
-                // For long-running tasks (10 seconds or more) use WorkManager.
-
-            } else {
-                // Handle message within 10 seconds
-
+            val id = remoteMessage.data["id"]
+            id?.let {
+                val detailIntent = DetailActivity.newIntent(this, id)
+                pendingIntent = PendingIntent.getActivity(
+                    this, 1, detailIntent, PendingIntent.FLAG_UPDATE_CURRENT
+                )
             }
         }
+
+        buildAndSendNotification(
+            this,
+            title ?: "FCM Testing",
+            body ?: "FCM Testing",
+            pendingIntent
+        )
 
     }
 
